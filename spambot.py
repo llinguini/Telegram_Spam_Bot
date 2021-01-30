af945b11dc0e5de61775d5167f0b691dc53b6222
linguini <42505763+llinguini@users.noreply.github.com>
2021-01-30 17:17:27 -0600

    Add files via upload

---
+++ spambot.py
@@ -0,0 +1,13 @@
+import botogram
+bot = botogram.create("AQUI VA EL TOKEN DEL BOT")
+bot.owner = "White Suit Hacking"
+
+@bot.command("spam")
+def spam_command(chat, message, args):
+	
+    for i in range (10000):
+        chat.send("*PON TU MENSAJE AQUI*")
+       
+
+if __name__ == "__main__":
+    bot.run()
